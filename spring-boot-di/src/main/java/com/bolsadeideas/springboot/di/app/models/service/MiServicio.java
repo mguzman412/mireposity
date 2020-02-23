b48a1395dd49280d1fc5cb1b0fc5eeb778f44c81
mguzman <mguzman412@gmail.com>
2020-02-23 02:35:18 -0300

    first commit

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -1,8 +1,10 @@
 package com.bolsadeideas.springboot.di.app.models.service;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Component("miServicioSimple")
+//@Component("miServicioSImple")
+//@Primary
 public class MiServicio implements IServicio{
 
 		@Override
--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicioComplejo.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicioComplejo.java
@@ -2,7 +2,8 @@ package com.bolsadeideas.springboot.di.app.models.service;
 
 import org.springframework.stereotype.Component;
 
-@Component("miServicioComplejo")
+//@Component("miServicioPrincipal")
+
 public class MiServicioComplejo implements IServicio{
 
 		@Override
